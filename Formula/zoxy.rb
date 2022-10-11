6d35eba7e215475130189d8580ee32487dca7f53
Amedee d'Aboville <amedee.daboville@gmail.com>
2022-10-11 12:08:05 -0400

    Rename app to zoxy

--- Formula/zoxy.rb
+++ Formula/zoxy.rb
@@ -1,7 +1,7 @@
-class Zproxy < Formula
+class Zoxy < Formula
   desc "Proxy local requests like localhost:8888 to jupyter.z"
-  homepage "https://gitlab.com/amedeedabo/zproxy"
-  url "https://gitlab.com/amedeedabo/zproxy/-/archive/v0.2.0/zproxy-v0.2.0.tar.gz"
+  homepage "https://gitlab.com/amedeedabo/zoxy"
+  url "https://gitlab.com/amedeedabo/zoxy/-/archive/v0.2.0/zoxy-v0.2.0.tar.gz"
   sha256 "9ff25a1795d2490dc2624038059c4401a4f7af7d62232025f98ee813f1df1cba"
   license "MIT"
 
@@ -12,17 +12,17 @@ class Zproxy < Formula
   end
 
   service do
-    error_log_path var/"log/zproxy.log"
-    log_path var/"log/zproxy.log"
+    error_log_path var/"log/zoxy.log"
+    log_path var/"log/zoxy.log"
 
     # TODO: don't keep alive if user config is broken
-    run bin/"zproxy"
+    run bin/"zoxy"
     keep_alive true
   end
 
   test do
     # TODO: run with bad config and expect it to stop itself
-    # system bin/"zproxy" "--config=<(bad_config)"
+    # system bin/"zoxy" "--config=<(bad_config)"
     # or add --noDNS and --proxyPort options to be able to
     # start it without sudo
     true
