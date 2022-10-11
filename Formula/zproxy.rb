38924b5743dee9f600e5ebaf4484a803d1a41190
Amedee d'Aboville <amedee.daboville@gmail.com>
2022-10-10 20:04:21 -0400

    Add brew service, update to v0.2

--- Formula/zproxy.rb
+++ Formula/zproxy.rb
@@ -1,14 +1,20 @@
 class Zproxy < Formula
   desc "Proxy local requests like localhost:8888 to jupyter.z"
   homepage "https://gitlab.com/amedeedabo/zproxy"
-  url "https://gitlab.com/amedeedabo/zproxy/-/archive/v0.1.0/zproxy-v0.1.0.tar.gz"
-  sha256 "486601263aa3c8d2ee7760617e0cd03a36f8b081971c745a00618aab2774fafc"
+  url "https://gitlab.com/amedeedabo/zproxy/-/archive/v0.2.0/zproxy-v0.2.0.tar.gz"
+  checksum "9ff25a1795d2490dc2624038059c4401a4f7af7d62232025f98ee813f1df1cba"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  service do
+    # TODO: don't keep alive if user config is broken
+    run bin/"zproxy"
+    keep_alive true
   end
 
   test do
